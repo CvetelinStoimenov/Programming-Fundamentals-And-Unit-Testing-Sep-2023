1d4b692cfff663a3578f99f4bb3f78302a4b194e
Mishenata <45821593+Mishenata@users.noreply.github.com>
2023-10-01 13:22:34 +0300

    01.10

---
+++ 11.01.NumberType/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+if (num == 0)
+{
+    Console.WriteLine("zero");
+}
+else if (num < 0)
+{
+    Console.WriteLine("negative");
+}
+else
+{
+    Console.WriteLine("positive");
+}
---
+++ 11.02.ATM/Program.cs
@@ -0,0 +1,16 @@
+﻿int balance = int.Parse(Console.ReadLine());
+int withdraw = int.Parse(Console.ReadLine());
+int limit = int.Parse(Console.ReadLine());
+
+if (balance >= withdraw && withdraw <= limit)
+{
+    Console.WriteLine("The withdraw was successful.");
+}
+else if (withdraw >= limit)
+{
+    Console.WriteLine("The limit was exceeded.");
+}
+else
+{
+    Console.WriteLine("Insufficient availability.");
+}
