b98e804a0c1f492416252d5f9d8bdfb9eb373495
Mishenata <45821593+Mishenata@users.noreply.github.com>
2023-10-17 10:35:19 +0300

    new

--- 17.08.MultiplicationSign/Program.cs
+++ 17.08.MultiplicationSign/Program.cs
@@ -17,3 +17,30 @@ Input	Output		Input	Output		Input	Output
 3                   3                   0
 -1                  1                   1*/
 
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+PrintSign(num1, num2, num3);
+
+static void PrintSign(int num1, int num2, int num3)
+{
+    if (num1 == 0 || num2 == 0 || num3 == 0)
+    {
+        Console.WriteLine("zero");
+    }
+    else if ((num1 > 0 && num2 > 0 && num3 > 0)
+          || (num1 > 0 && num2 < 0 && num3 < 0)
+          || (num1 < 0 && num2 > 0 && num3 < 0)
+          || (num1 < 0 && num2 < 0 && num3 > 0))
+    {
+        Console.WriteLine("positive");
+    }
+    else if ((num1 < 0 && num2 < 0 && num3 < 0)
+          || (num1 < 0 && num2 > 0 && num3 > 0)
+          || (num1 > 0 && num2 < 0 && num3 > 0)
+          || (num1 > 0 && num2 > 0 && num3 < 0))
+    {
+        Console.WriteLine("negative");
+    }
+}
---
+++ 17.09.PasswordValidator/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+ 9.	Password Validator
+Write a program that checks if a given password is valid.
+The password validation rules are:
+    •	It should contain 6 – 10 characters (inclusive)
+    •	It should contain only letters and digits
+    •	It should contain at least 2 digits 
+If it is not valid, for any unfulfilled rule print the corresponding message:
+    •	"Password must be between 6 and 10 characters"
+    •	"Password must consist only of letters and digits"
+    •	"Password must have at least 2 digits"
+
+Example Input / Output
+
+Input	    Output
+logIn       Password must be between 6 and 10 characters 
+            Password must have at least 2 digits
+
+MyPass123   Password is valid
+
+Pa$s$s      Password must consist only of letters and digits
+            Password must have at least 2 digits*/
+
+string pass = Console.ReadLine();
+bool isValidLenght = pass.Length >= 6 && pass.Length <= 10;
+bool isValidContent = CheckCountent(pass);
+bool isValidCountDigits = CheckCountDigits(pass);
+
+if (isValidLenght == true && isValidContent == true && isValidCountDigits == true)
+{
+    Console.WriteLine("Password is valid");
+}
+else
+{
+    if (isValidLenght == false)
+    {
+        Console.WriteLine("Password must be between 6 and 10 characters");
+    }
+
+    if (isValidContent == false)
+    {
+        Console.WriteLine("Password must consist only of letters and digits");
+    }
+
+    if (isValidCountDigits == false)
+    {
+        Console.WriteLine("Password must have at least 2 digits");
+    }
+}
+
+static bool CheckCountent(string pass)
+{
+    for (int position = 0; position < pass.Length; position++)
+    { 
+    char symbol = pass[position];
+        if(char.IsLetterOrDigit(symbol) == false) 
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool CheckCountDigits(string pass)
+{
+    int count = 0;
+    for (int position = 0; position < pass.Length; position++)
+    {
+        char symbol = pass[position];
+        if (char.IsDigit(symbol) == true)
+        {
+            count++;
+        }
+    }
+    return count >= 2;
+}
