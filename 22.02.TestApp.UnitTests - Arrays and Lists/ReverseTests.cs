9d954bcc1663c2ac91930672bd9d56d6fcd577a3
Mishenata <45821593+Mishenata@users.noreply.github.com>
2023-10-28 13:39:32 +0300

    Add unit tests

---
+++ 22.02.TestApp.UnitTests - Arrays and Lists/ReverseTests.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class ReverseTests
+{
+    [Test]
+    public void Test_ReverseArray_InputIsEmpty_ShouldReturnEmptyString()
+    {
+        // Arrange
+        int[] emptyArray = Array.Empty<int>();
+
+        // Act
+        string result = Reverse.ReverseArray(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+
+    // TODO: finish the test
+    [Test]
+    public void Test_ReverseArray_InputHasOneElement_ShouldReturnTheSameElement()
+    {
+        // Arrange
+
+        // Act
+        string result;
+
+        // Assert
+        Assert.That(result, Is.EqualTo("42"));
+    }
+
+    [Test]
+    public void Test_ReverseArray_InputHasMultipleElements_ShouldReturnReversedString()
+    {
+        // TODO: finish the test
+    }
+}
---
+++ 23.02.TestApp.UnitTests - Methods, Arrays, Lists/FibonacciTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class FibonacciTests
+{
+    [Test]
+    public void Test_CalculateFibonacci_NegativeInput()
+    {
+        // Arrange
+        int n = -1;
+
+        // Act & Assert
+        Assert.That(() => Fibonacci.CalculateFibonacci(n), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void Test_CalculateFibonacci_ZeroInput()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_CalculateFibonacci_PositiveInput()
+    {
+        // TODO: finish the test
+    }
+}
