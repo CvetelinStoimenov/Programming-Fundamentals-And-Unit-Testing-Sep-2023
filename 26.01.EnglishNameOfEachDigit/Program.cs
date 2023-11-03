690d9bf41b9804d038114637836c955fe81274dd
Mishenata <45821593+Mishenata@users.noreply.github.com>
2023-11-03 09:46:43 +0200

    03.11.2023

--- 26.01.EnglishNameOfEachDigit/Program.cs
+++ 26.01.EnglishNameOfEachDigit/Program.cs
@@ -34,4 +34,48 @@ Input	Output
         six
         one*/
 
+int input = int.Parse(Console.ReadLine());
 
+while (input != 0)
+{
+    int digit = input % 10;
+
+    if (digit == 1)
+    {
+        Console.WriteLine("one");
+    }
+    else if (digit == 2) 
+    {  
+        Console.WriteLine("two"); 
+    }
+    else if (digit == 3) 
+    { 
+        Console.WriteLine("three"); 
+    }
+    else if (digit == 4) 
+    {  
+        Console.WriteLine("four"); 
+    }
+    else if (digit == 5) 
+    { 
+        Console.WriteLine("five"); 
+    }
+    else if (digit == 6) 
+    { 
+        Console.WriteLine("six"); 
+    }
+    else if (digit == 7) 
+    { 
+        Console.WriteLine("seven"); 
+    }
+    else if (digit == 8) 
+    {  
+        Console.WriteLine("eight"); 
+    }
+    else if (digit == 9) 
+    { 
+        Console.WriteLine("nine"); 
+    }
+
+    input = input / 10;
+}
